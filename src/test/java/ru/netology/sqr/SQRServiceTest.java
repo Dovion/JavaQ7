65b6345bcd0256dc1eac7caa55a737a0f069294e
Greds <vyacheslav-98@yandex.ru>
2021-09-22 03:28:26 +0300

    Formatted

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,17 +2,18 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
-        @ParameterizedTest
-        @CsvSource(value = {
+    @ParameterizedTest
+    @CsvSource(value = {
             "200,300,3"
-        }
-        )
-        void ShouldCalculateSQR (int squareUnder,int squareOver, int expected) {
-            SQRService service = new SQRService();
-            int actual = service.SQRCalc(squareUnder, squareOver);
-            assertEquals(expected, actual);
-        }
+    }
+    )
+    void ShouldCalculateSQR(int squareUnder, int squareOver, int expected) {
+        SQRService service = new SQRService();
+        int actual = service.SQRCalc(squareUnder, squareOver);
+        assertEquals(expected, actual);
+    }
 }
